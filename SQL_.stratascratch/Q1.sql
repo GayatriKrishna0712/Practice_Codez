a355e10e1ba43ca435f6c9a3b4bbe73f81bf080f
Gayatri Krishna <93417245+GayatriKrishna0712@users.noreply.github.com>
2023-04-10 15:42:17 +0530

    Update Q1.sql

--- SQL_.stratascratch/Q1.sql
+++ SQL_.stratascratch/Q1.sql
@@ -1,1 +1,7 @@
+# Find the number of reviews received by Lo-Lo's Chicken & Waffles for each star. Output the number of stars along with the corresponding number of reviews. Sort records by stars in ascending order.
+select 
+   stars,
+   COUNT(*) OVER(partition by stars order by stars ASC) as reviews_received
+from yelp_reviews
+where business_name like '%Lo-Lo%';
 
