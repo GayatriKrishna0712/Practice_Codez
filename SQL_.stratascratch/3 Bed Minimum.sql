fc794a58c8ec8e91dfcbd080e86af646b47fa140
Gayatri Krishna <93417245+GayatriKrishna0712@users.noreply.github.com>
2023-04-14 09:55:51 +0530

    Create 3 Bed Minimum.sql

---
+++ SQL_.stratascratch/3 Bed Minimum.sql
@@ -0,0 +1,9 @@
+#Find the average number of beds in each neighborhood that has at least 3 beds in total. Output results along with the neighborhood name and sort the results based on the number of average beds in descending order.
+
+select  DISTINCT
+   neighbourhood,
+   ROUND(AVG(bedrooms),0) as avg_bed
+from airbnb_search_details
+WHERE neighbourhood is not null
+GROUP BY neighbourhood
+ORDER BY avg_bed DESC;
