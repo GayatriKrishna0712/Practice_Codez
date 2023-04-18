172fed5ab3afe2216337822821182bb611e4c852
Gayatri Krishna <93417245+GayatriKrishna0712@users.noreply.github.com>
2023-04-18 15:48:57 +0530

    Update Finding User Purchases.sql

--- SQL_.stratascratch/Finding User Purchases.sql
+++ SQL_.stratascratch/Finding User Purchases.sql
@@ -1,10 +1,9 @@
 # Write a query that'll identify returning active users. A returning active user is a user that has made a second purchase within 7 days of any other of their purchases. Output a list of user_ids of these returning active users.
 
-SELECT DISTINCT
-   a.user_id
+select distinct
+    a.user_id
 from amazon_transactions a
-JOIN amazon_transactions b 
-     ON a.user_id = b.user_id 
-  WHERE a.created_at - b.created_at between 0 and 7
-  AND a.id != b.id
-ORDER by 1 ASC;
+JOIN amazon_transactions b on a.user_id = b.user_id
+WHERE a.created_at-b.created_at between 0 and 7
+AND a.id<>b.id
+ORDER BY 1 ASC;
